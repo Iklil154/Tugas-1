b4e5d3694d856c4356ed4fe67c57aadc659680e4
Iklil154 <143860592+Iklil154@users.noreply.github.com>
2023-09-05 01:13:53 +0700

    Add files via upload

---
+++ volume balok.py
@@ -0,0 +1,6 @@
+panjang = int(input('masukkan panjang : '))
+lebar = int(input('masukkan lebar : '))
+tinggi = int(input('masukkan tinggi : '))
+
+volume = int(panjang * lebar * tinggi)
+print("volume balok :",volume)
---
+++ volume bola.py
@@ -0,0 +1,7 @@
+print("Menghiitung luas dan volume bola")
+r = int(input("Masukkan Jari - Jari = "))
+phi = 22/7
+luas = 4/3*(phi*r*r*r)
+volume = 4*phi*r*r
+print("luas bola adalah = ",luas)
+print("volume bola adalah = ",volume)
